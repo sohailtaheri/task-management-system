e1f37510cb9330f445526d0f55e202383d90ad25
Sohail Taheri <shl.taheri@gmail.com>
2025-04-09 21:20:54 +0100

    postgres connection

--- task_management_system/settings.py
+++ task_management_system/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'task_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'template1',
+        'USER': 'postgres',
+        'PASSWORD': '842390',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
